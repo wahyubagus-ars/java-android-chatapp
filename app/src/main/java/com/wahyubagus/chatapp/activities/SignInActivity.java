0f8ce742c2532a95b3f01186e247fc281ea02a51
wahyubagus <wahyu.sulaksono@ist.id>
2021-11-04 23:56:49 +0700

    fixing night mode and add dependencies firebase

--- app/src/main/java/com/wahyubagus/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/wahyubagus/chatapp/activities/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.wahyubagus.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListener();
