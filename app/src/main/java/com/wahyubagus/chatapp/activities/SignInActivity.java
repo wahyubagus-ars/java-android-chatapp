a0b9e786ba739bf67529b73425d3cd5aba409f17
wahyubagus <wahyu.sulaksono@ist.id>
2021-11-05 00:50:51 +0700

    setup: setup firebase

--- app/src/main/java/com/wahyubagus/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/wahyubagus/chatapp/activities/SignInActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.wahyubagus.chatapp.R;
 import com.wahyubagus.chatapp.databinding.ActivitySignInBinding;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -26,6 +30,28 @@ public class SignInActivity extends AppCompatActivity {
         binding.textCreateNewAccount.setOnClickListener(v -> {
             startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
         });
+
+        //binding.buttonSignIn.setOnClickListener(view -> addDataFirebase());
     }
+
+    /*
+    * This method just for test that configure firebase correct or not
+    * TODO: Delete this method when use real case with fire base
+    * */
+    /*private void addDataFirebase(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "wahyu");
+        data.put("last_name", "bagus");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }*/
 
 }
