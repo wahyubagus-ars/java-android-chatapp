0dd86ec41f32464c9bf566397191e7152c2bd093
wahyubagus <wahyu.sulaksono@ist.id>
2021-11-14 22:09:03 +0700

    feature: design chat app

---
+++ app/src/main/java/com/wahyubagus/chatapp/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.wahyubagus.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wahyubagus.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
